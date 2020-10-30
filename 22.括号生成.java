5f391c44caeacf3cc8b22b59ad05f80e617b9876
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2020-10-30 08:36:19 +0800

    Create 22.括号生成.java

---
+++ 22.括号生成.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res=new ArrayList<String>();
+        generate(res,"",0,0,n);
+        return res;
+    }
+    public void generate(List<String> res,String ans,int count1,int count2,int n){
+        if(count1>n||count2>n) return;
+        if(count1==n&&count2==n) res.add(ans);
+        if(count1>=count2){
+            String ans1=new String(ans);
+            generate(res,ans+"(",count1+1,count2,n);
+            generate(res,ans1+")",count1,count2+1,n);
+        }
+    }
+}
