348f80eb2065f85fc7feffe60066aceb54b3d129
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2020-11-09 08:53:05 +0800

    Create 94.二叉树的中序遍历.java

---
+++ 94.二叉树的中序遍历.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        order(root,res);
+        return res;
+    }
+    public void order(TreeNode root,List<Integer> res){
+        if(root==null) return;
+        order(root.left,res);
+        res.add(root.val);
+        order(root.right,res);
+
+    }
+}
