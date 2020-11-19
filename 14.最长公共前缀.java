ae92749c94bbaa06cba970c2ca2776f22e26b057
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2020-11-19 08:38:51 +0800

    Create 14.最长公共前缀.java

---
+++ 14.最长公共前缀.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        if(strs.length==1) return strs[0];
+        String s=strs[0];
+        int max=0;
+        for(int i=0;i<s.length();++i){
+            int def=0;
+            char ch=s.charAt(i);
+            for(int j=1;j<strs.length;++j){
+                if(i>=strs[j].length()) break;
+                char ch2=strs[j].charAt(i);
+                if(ch==ch2) def++;
+            }
+            if(def==strs.length-1) max=i+1;
+            else break;
+        }
+        if(max==0) return "";
+        else return s.substring(0,max);
+    }
+}
