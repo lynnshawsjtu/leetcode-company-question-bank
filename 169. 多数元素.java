45221557448d0fef952692caf64222ec57ed5f23
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2020-12-29 08:42:06 +0800

    Create 169. 多数元素.java

---
+++ 169. 多数元素.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        int count=1;
+        int res=nums[0];
+        for(int i=1;i<=n-1;++i){
+            if(nums[i]==res) count++;
+            else {
+                count--;
+                if(count==0){
+                    count=1;
+                    res=nums[i];
+                }
+            }
+        }
+        return res;
+    }
+}
