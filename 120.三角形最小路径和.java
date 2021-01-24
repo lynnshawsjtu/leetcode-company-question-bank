b663708f6a3d448669f35f9a49e041a8af973ffa
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2021-01-24 08:01:21 +0800

    Create 120.三角形最小路径和.java

---
+++ 120.三角形最小路径和.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle==null||triangle.size()==0) return 0;
+        int[][] dp=new int[triangle.size()+1][triangle.size()+1];
+
+        for(int i=triangle.size()-1;i>=0;i--){
+            List<Integer> curTr=triangle.get(i);
+            for(int j=0;j<curTr.size();++j){
+                dp[i][j]=Math.min(dp[i+1][j],dp[i+1][j+1])+curTr.get(j);
+            }
+        }
+        //从下往上dp
+        return dp[0][0];
+    }
+}
