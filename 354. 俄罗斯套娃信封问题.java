e82b1837bff0a342e072d9b90019b8e977d87b0e
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2021-03-01 09:03:24 +0800

    Create 354. 俄罗斯套娃信封问题.java

---
+++ 354. 俄罗斯套娃信封问题.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        int n=envelopes.length;
+        if(n<1) return 0;
+        int max=1;
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        Arrays.sort(envelopes,(a,b)->(a[0]-b[0]));
+        //dp
+        for(int i=1;i<n;++i){
+            for(int j=0;j<i;++j){
+                if(envelopes[i][0]>envelopes[j][0]&&envelopes[i][1]>envelopes[j][1])
+                dp[i]=Math.max(dp[i],dp[j]+1);
+            }
+            max=Math.max(max,dp[i]);
+        }
+        return max;
+    }
+}
