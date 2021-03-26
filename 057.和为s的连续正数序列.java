f5f33c2051ef6100fab605995aa19f748a69e608
Lynn Xiao <49852167+lynnshawsjtu@users.noreply.github.com>
2021-03-26 08:39:15 +0800

    Create 057.和为s的连续正数序列.java

---
+++ 057.和为s的连续正数序列.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> list=new ArrayList<>();
+        for(int l=1,r=1,sum=0;r<target;++r){
+            sum+=r;
+            while(sum>target) sum-=l++;
+            if(sum==target){
+                int[] temp=new int[r-l+1];
+                for(int i=0;i<temp.length;++i){
+                    temp[i]=l+i;
+                }
+                list.add(temp);
+            }
+        }
+        int[][] res=new int[list.size()][];
+        for(int i=0;i<res.length;++i){
+            res[i]=list.get(i);
+        }
+        return res;
+    }
+}
